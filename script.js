7b0543a110c8231bb4ded26eeb48306679fb3c92
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-06-14 11:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,10 +16,18 @@ const library = [
   },
 ];
 
-const numberOfBooksRead = () => {
+const numberOfBooksRead = (library) => {
   // write your code here
+	let count=0;
+	for(let i=0;i<library.length;i++)
+		{
+			if(library[i].readingStatus===true){
+				count++;
+			}
+		}
+	return count;
 };
 
 // Do not change the code below
 
-alert(numberOfBooksRead());
+ alert(numberOfBooksRead());
